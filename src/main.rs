5731a51a30a8d4619ac07ac4ca32d67961b198f0
Moof Soup <moofsoup@gmail.com>
2024-10-05 10:21:22 -0700

    first implementation of the super meow tbh thingy

--- src/main.rs
+++ src/main.rs
@@ -1,32 +1,76 @@
 
-    fn contains_duplicate(nums: Vec<i32>) -> bool {
-        let mut stack: Vec<i32> = Vec::new();
-        
-        for i in nums.iter(){
-            if stack.is_empty() == true {
-                stack.push(i.clone());
-            } else {
-                // compare to each on the stack
-                for k in 1..=stack.len(){
-                    if nums[i.clone() as usize] == stack[k]{
-                        return true
-                    }
-                }
-                stack.push(i.clone());
-            }
-            
-            // how do I compare the first index in the vector to the stack
-            // compare the value to the values on the stack
-            
-            // push the value to the stack
-        }
+struct HanoiIterator{
+    state: Vec<(usize, usize, usize, usize)>,
 
-        false
+}impl HanoiIterator {
+    fn new(n: usize) -> Self {
+        HanoiIterator {
+            state: vec![(n, 0, 1, 2)],
+        }
+    }
+}
+
+impl Iterator for HanoiIterator {
+    type Item = (usize, usize);
+
+    fn next(&mut self) -> Option<Self::Item> {
+        while let Some((n, from, aux, to)) = self.state.pop() {
+            if n == 1 {
+                return Some((from, to));
+            } else {
+                self.state.push((n - 1, aux, from, to));
+                self.state.push((1, from, aux, to));
+                self.state.push((n - 1, from, to, aux));
+               
+            }
+        }
+        None
+    }
+}
+struct CompAllIterator {
+    stack: Vec<(usize,usize, usize)>,
+}
+impl CompAllIterator {
+    fn new(n: usize)-> CompAllIterator {
+        CompAllIterator {
+            stack: vec! {(n, n, n - 1)}
+        }
+    }
+}
+impl Iterator for CompAllIterator {
+    type Item = (usize, usize);
+    fn next(&mut self) -> Option<Self::Item> {
+        // if the base case n == 1
+        while let Some((n, first_operand, second_operand)) = self.stack.pop() {
+            if n == 1 {
+                return Some((0, 1));
+            } else {
+                // make a recursive call
+                // return the value
+                self.stack.push((n - 1, first_operand - 1, second_operand - 1));
+                self.stack.push((1, first_operand, second_operand));
+            
+            }
+        }
+        None
+
+        // return Some((0, 1));
+
+    }
+
+
+
+}fn contains_duplicate(nums: Vec<i32>) -> bool {
         
+        true
     }
 
     fn main(){
-
+       let w_dupes = vec!{1, 2, 3, 3};
+        let comp = CompAllIterator::new(w_dupes.len());
+        for (left_operand, right_operand) in comp{
+            println!("hello {left_operand} {right_operand}" )
+        }
     }
 
 
